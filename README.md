4bdfa658b6d5e2bfbd2065c26771254ca58307f1
AlexE <zed-chi@mail.ru>
2018-08-29 22:45:09 +0300

    init

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+Script prints response status of sites and expire date of domain 
 
-# Project Goals
+
+## Usage
+* You need *.txt file with urls list
+* python 3.5 is required to run script
+* install requirement modules:
+```bash
+$ pip install -r requirements.txt
+```
+* Run:
+
+```bash
+$ python check_sites_health.py -p <path_to_textfile>
+```
+output will be like:
+```bash
+<url1> - Respond, expires 2019-07-31 21:00:00
+<url2> - Respond, expires 2019-07-31 21:00:00
+<url3> - NOT respond, expires <no date>
+<url4> - Respond, expires 2019-07-31 21:00:00
+```
+
+### Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,57 @@
-def load_urls4check(path):
-    pass
+import argparse
+import whois
+import requests
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        return requests.get(url).status_code == 200
+    except requests.exceptions.ConnectionError as err:
+        return False
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def get_domain_expiration_date(url):
+    domain = whois.extract_domain(url)
+    return whois.whois(domain).expiration_date
+
+
+def get_path():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", required=True, dest="file_path")
+    args = parser.parse_args()
+    return args
+
+
+def read_urls_from_file(path):
+    with open(path) as textfile:
+        urls = textfile.readlines()
+    return list(map(lambda x: x.rstrip(), urls))
+
+
+def get_url_statuses(urls):
+    statuses = []
+    for url in urls:
+        if is_server_respond_with_200(url):
+            status = "Respond"
+        else:
+            status = "NOT respond"
+        expires = get_domain_expiration_date(url) or "<no date>"
+        statuses.append([status, expires])
+    return zip(urls, statuses)
+
+
+def print_urls_info(urls_info):
+    for url_info in urls_info:
+        url, [stat, exp] = url_info
+        print("{} - {}, expires {}".format(url, stat, exp))
+
+
+def main():
+    path = get_path().file_path or exit("empty path")
+    urls = read_urls_from_file(path)
+    url_with_statuses = get_url_statuses(urls)
+    print_urls_info(url_with_statuses)
+
 
 if __name__ == '__main__':
-    pass
+    main()
